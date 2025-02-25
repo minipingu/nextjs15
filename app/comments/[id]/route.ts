6ba235f605f8b5515a88ce338bd0dead7b216b57
minipingu <ranmaniax@gmail.com>
2025-02-25 20:52:37 +0700

    delete

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -1,5 +1,5 @@
 import { comments } from '../data'
-
+//to use params, u need to use request: Request, also
 export async function GET(
 	request: Request,
 	{ params }: { params: Promise<{ id: string }> }
@@ -28,4 +28,21 @@ export async function PATCH(
 	comments[index].text = text
 	//6. returning response
 	return Response.json(comments[index])
+}
+
+export async function DELETE(
+	request: Request,
+	{ params }: { params: Promise<{ id: string }> }
+) {
+	//extract id from parameters
+	const { id } = await params
+	//finding index
+	const index = comments.findIndex((comment) => comment.id === Number(id))
+	//removing comment from comments array
+	comments.splice(index, 1)
+	//
+	return Response.json({
+		message: `Comment deleted successfully`,
+		comments,
+	})
 }
