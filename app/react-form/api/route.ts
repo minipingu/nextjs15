5f02edf574ab5a99748e62e9285fbf475b50aca3
minipingu <ranmaniax@gmail.com>
2025-03-01 04:27:43 +0700

    form input

---
+++ app/react-form/api/route.ts
@@ -0,0 +1,13 @@
+import { addProduct } from '@/app/prismadb-crud'
+
+export async function POST(request: Request) {
+	const body = await request.json()
+
+	const { title, price, description } = body
+
+	const product = await addProduct(title, parseInt(price), description)
+
+	return new Response(JSON.stringify(product), {
+		headers: { 'Content-Type': 'application/json' },
+	})
+}
